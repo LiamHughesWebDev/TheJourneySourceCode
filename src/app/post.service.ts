4243ed43402d3b7880247f70d05c7200ee185151
ljhughes599 <ljhughes599@gmail.com>
2023-12-16 12:26:29 -0500

    Updated Font & Added Navigation

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -12,35 +12,35 @@ export class PostService {
   public posts = [
     {
       id: 0,
-      title:"Post #0",
+      title:"Post 0",
       filter: "none",
       summary: "Card text that is taking up a fair bit of space so that I can see what it would kinda look like when I actually have something usefull here",
       content: "Card text that is taking up a fair bit of space so that I can see what it would kinda look like when I actually have something usefull here. Card text that is taking up a fair bit of space so that I can see what it would kinda look like when I actually have something usefull here"
     },
     {
       id: 1,
-      title:"Post #1",
+      title:"Post 1",
       filter: "none",
       summary: "Card text that is taking up a fair bit of space so that I can see what it would kinda look like when I actually have something usefull here",
       content: "Card text that is taking up a fair bit of space so that I can see what it would kinda look like when I actually have something usefull here. Card text that is taking up a fair bit of space so that I can see what it would kinda look like when I actually have something usefull here"
     },
     {
       id: 2,
-      title:"Post #2",
+      title:"Post 2",
       filter: "none",
       summary: "Card text that is taking up a fair bit of space so that I can see what it would kinda look like when I actually have something usefull here",
       content: "Card text that is taking up a fair bit of space so that I can see what it would kinda look like when I actually have something usefull here. Card text that is taking up a fair bit of space so that I can see what it would kinda look like when I actually have something usefull here"
     },
     {
       id: 3,
-      title:"Post #3",
+      title:"Post 3",
       filter: "none",
       summary: "Card text that is taking up a fair bit of space so that I can see what it would kinda look like when I actually have something usefull here",
       content: "Card text that is taking up a fair bit of space so that I can see what it would kinda look like when I actually have something usefull here. Card text that is taking up a fair bit of space so that I can see what it would kinda look like when I actually have something usefull here"
     },
     {
       id: 4,
-      title:"Post #4",
+      title:"Post 4",
       filter: "none",
       summary: "Card text that is taking up a fair bit of space so that I can see what it would kinda look like when I actually have something usefull here",
       content: "Card text that is taking up a fair bit of space so that I can see what it would kinda look like when I actually have something usefull here. Card text that is taking up a fair bit of space so that I can see what it would kinda look like when I actually have something usefull here"
