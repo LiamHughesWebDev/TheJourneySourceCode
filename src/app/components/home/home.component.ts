dd675d7ddd18f6e4af40324673d7cf34e8f4eff7
ljhughes599 <ljhughes599@gmail.com>
2023-12-20 22:47:33 -0500

    Designed Comment Section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -22,22 +22,5 @@ export class HomeComponent implements OnInit{
     
     
   }
-  tiltEffect(e:MouseEvent){
-    //get event
-    const EventTarget:any = e.target;
-    const card:any = EventTarget.getBoundingClientRect();
-    //get card's width and height
-    const cardWidth = card.width;
-    const cardHeight = card.height;
 
-    //gets mouse position relative to the specific card by determining the mouse's position on hovering over the card, and subtracting the x & y positions from the card's position. 
-    var mouseX = e.clientX - card.left;
-    var mouseY = e.clientY - card.top;
-
-    //turn relative mouse position into a percentage
-    var mouseXPercent = mouseX / cardWidth;
-    var mouseYPercent = mouseY / cardHeight;
-
-    console.log({mouseXPercent, mouseYPercent})
-  }
 }
